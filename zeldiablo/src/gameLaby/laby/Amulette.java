ce3991795e9bb075bc3924311314fe717016aec4
Dirand Felicien <felicien.dirand1@gmail.com>
2023-05-31 15:29:42 +0200

    Amulette

---
+++ zeldiablo/src/gameLaby/laby/Amulette.java
@@ -0,0 +1,53 @@
+package gameLaby.laby;
+
+public class Amulette implements Entite {
+    public final static String AMULETTE = "Amulette";
+    /**
+     * position de l'amulette
+     */
+    int x, y;
+
+    /**
+     * constructeur de l'amulette
+     *
+     * @param dx position selon x
+     * @param dy position selon y
+     */
+    public void Entite(int dx, int dy) {
+        this.x = dx;
+        this.y = dy;
+    }
+
+    /**
+     * permet de savoir si l'amulette est en x,y
+     *
+     * @param dx position testee
+     * @param dy position testee
+     * @return true si le monstre est bien en (dx,dy)
+     */
+    public boolean etrePresent(int dx, int dy) {
+
+        return (this.x == dx && this.y == dy);
+    }
+
+    // ############################################
+    // GETTER
+    // ############################################
+
+    /**
+     * @return position x de l'amulette
+     */
+    public int getX() {
+        // getter
+        return this.x;
+    }
+
+    /**
+     * @return position y de l'amulette
+     */
+    public int getY() {
+        //getter
+        return this.y;
+    }
+
+}
