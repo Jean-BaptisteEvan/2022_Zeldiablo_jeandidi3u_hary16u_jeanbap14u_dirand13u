6ff308fff5b5bce8a392ba9d7ca1133324f30fa4
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2023-05-31 16:01:47 +0200

    ajout test de mouvement monstre et perso

--- zeldiablo/test/TestIterationUn.java
+++ zeldiablo/test/TestIterationUn.java
@@ -1,4 +1,5 @@
 import gameLaby.laby.LabyJeu;
+import gameLaby.laby.Labyrinthe;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,10 +13,31 @@ public class TestIterationUn {
         try {
             LabyJeu jeuLab = new LabyJeu("labySimple/labytestMouvP.txt",0,0);
             boolean bPresence = jeuLab.getLabyrinthe().monstre.etrePresent(3,2);
-            assertEquals(true,true,"le monstre devrait exister");
+            assertEquals(true,bPresence,"le monstre devrait exister");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+    }
+    @Test
+    public void test_MouvAleatoireMonstre(){
+        try {
+            LabyJeu jeuLab = new LabyJeu("labySimple/labytestMouvP.txt",0,0);
+            jeuLab.getLabyrinthe().deplacerEntite(Labyrinthe.GAUCHE);
+            boolean bPresence = jeuLab.getLabyrinthe().monstre.etrePresent(3,2);
+            assertEquals(false,bPresence,"le monstre devrait avoir bouger");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void test_PersoMouvContreMonstre(){
+        try {
+            LabyJeu jeuLab = new LabyJeu("labySimple/labytestMouvP.txt",0,0);
+            jeuLab.getLabyrinthe().deplacerEntite(Labyrinthe.GAUCHE);
+            boolean bPresence = jeuLab.getLabyrinthe().pj.etrePresent(2,2);
+            assertEquals(false,bPresence,"le monstre devrait avoir bouger");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
